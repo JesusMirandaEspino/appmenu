df94b93bda0c8e0d43f1faac01367ecec2c643f0
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-04-03 11:13:33 -0600

    main & header general styles

--- src/app/services/apifood.service.ts
+++ src/app/services/apifood.service.ts
@@ -5,13 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class ApifoodService {
 
-  apiKey = '7e10d42d6c3d73d1d9c1e3e010abec0a';
-  apiId = '743996d6';
-  apiStatus = 'live';
-  apiPlan = 'Developer';
   apiEspenocular = '9a31b7bf6ff94359885c50e84ecee992';
   url = 'https://api.spoonacular.com/food/menuItems/search?query=burger&number=2&apiKey=9a31b7bf6ff94359885c50e84ecee992';
 
+
+  // https://spoonacular.com/food-api/docs#Search-Menu-Items
   // query valor buscado
   // number cantidad a mostrar
   // apiKey clave de acceso a la API
